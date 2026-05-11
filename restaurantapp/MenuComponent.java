0ef191420104c32f8508918089dc352f3d45ebbf
sara21zu <sarazu21s@gmail.com>
2026-05-11 16:25:09 +0300

    Add files via upload

---
+++ restaurantapp/MenuComponent.java
@@ -0,0 +1,11 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package restaurantapp;
+
+public interface MenuComponent {
+    void display(int indent);
+    double getPrice();
+    String getName();
+}
---
+++ restaurantapp/RestaurantApp.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package restaurantapp;
+public class RestaurantApp {
+public static void main(String[] args) {
+        MenuItem burger = new MenuItem("Zinger Burger" , 3.0 );
+        MenuItem pizza = new MenuItem("Pizza Margherita" , 5.0 );
+        
+        Menu meal = new Menu("Family Meal");
+        meal.add(burger);
+        meal.add(new MenuItem("Large Fries", 1.5));
+        meal.add(new MenuItem("Coke", 1.0));
+        
+        Menu mainMenu = new Menu("Main Menu");
+        mainMenu.add(pizza);
+        mainMenu.add(meal);
+        
+        mainMenu.display(0);
+        System.out.println("\nGrand Total: " + mainMenu.getPrice() + "JOD");
+    }
+    
+}
